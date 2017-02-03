3e6196bab2f42f93c347356b79238aea9d8b4f96
khar <khar@gmail.com>
2017-02-03 09:00:19 +0300

    03.02.17

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int x, y, sum, pr, razn, chastn; //объ€вление
+
+    setlocale(LC_ALL, "Russian"); //руссификаци€ командной строки
+
+    cout << "¬ведите два целых числа через пробел "; //приглашение
+    cin >> x >> y; //прочитать два целых числа
+    sum = x + y;
+    cout << "—умма чисел\n" << sum;
+
+    getchar();
+    return 0; //показывает, что программа успешно завершена
 }
